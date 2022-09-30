7213ac2c92f9b1676e7e889e14ee68b6dd215159
kavyahallur123 <kavyahallur199@gmail.com>
2022-09-30 14:04:54 +0530

    Added:Employee wage

---
+++ employeewage/DailyWage.java
@@ -0,0 +1,15 @@
+package com.bridgelab.employeewage;
+public class DailyWage {
+        public static void main(String[] args) {
+            int IS_PRESENT = 1, EMP_RATE_PER_HOUR = 20;
+            int emphrs = 0, empwage = 0;
+            double employeeCheck = Math.floor(Math.random() * 10) % 2;
+            if ( employeeCheck == 1)
+                emphrs = 8;
+            else
+                emphrs = 0;
+            empwage = emphrs * EMP_RATE_PER_HOUR ;
+            System.out.println("Employee wage is " + empwage);
+        }
+    }
+
